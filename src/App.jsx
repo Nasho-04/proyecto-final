f0447aae1b9fd7a92531cef39a7cca6de4a6a0f4
Nacho <nachonieva04@gmail.com>
2024-08-01 10:16:03 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route path='/' />
+        <Route />
+        <Route />
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ src/GlobalContext.jsx
@@ -0,0 +1,20 @@
+import { createContext, useContext } from "react";
+
+const GlobalContext = createContext();
+
+export const GlobalContextProvider = ({ children }) => {
+
+    return (
+        <GlobalContext.Provider value={
+            {
+                
+            }
+        }>
+            {children}
+        </GlobalContext.Provider>
+    )
+}
+
+export const useGlobalContext = () => {
+    return useContext(GlobalContext)
+}
